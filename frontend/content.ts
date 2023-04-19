ea3165922907d520fa5623fe00c303dcc2a59895
Beket Bayemirov <bayemirov@gmail.com>
2023-04-19 14:22:58 +0200

    New design + new parsing method

--- frontend/content.ts
+++ frontend/content.ts
@@ -2,6 +2,7 @@ import type { PlasmoCSConfig } from "plasmo"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://meet.google.com/*"],
+  css: ["./styles.css"],
   all_frames: true
 }
 
@@ -10,19 +11,21 @@ let observer: MutationObserver | undefined;
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'startObserving') {
     // Unique div class for google CC
-    const targetNode = document.querySelector('div.a4cQT');
+    const targetNode = document.querySelector('div.iOzk7');
     
     if (targetNode && !observer) {
       observer = new MutationObserver(mutations => {
         mutations.forEach((mutation) => {
           if (mutation.type === "childList") {
-            mutation.addedNodes.forEach((node) => {
-              if (node.nodeType === Node.TEXT_NODE) {
-                const textContent = node.textContent.trim();
-                chrome.runtime.sendMessage({ message: "storeText", content: textContent }, (response) => {
-                  console.log(response.message);
-                });
-              }
+            const targetNode = mutation.target;
+            const removedSpans = [];
+            mutation.removedNodes.forEach((node) => {
+              if (node.nodeName === 'SPAN')
+                removedSpans.push(node.textContent.trim());
+            });
+            removedSpans.reverse();
+            chrome.runtime.sendMessage({ message: "storeText", content: removedSpans.join(' ') }, (response) => {
+              console.log(response.message);
             });
           }
         });
@@ -30,13 +33,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       
       const observerConfig = { attributes: true, childList: true, subtree: true };
       observer.observe(targetNode, observerConfig);
-      sendResponse({ message: 'STARTED OBSERVING!' });
+      sendResponse({ message: 'started observing!' });
     }
   } else if (request.message === 'stopObserving') {
     if (observer) {
       observer.disconnect();
       observer = undefined;
-      sendResponse({ message: 'STOPPED OBSERVING!' });
+      sendResponse({ message: 'stopped observing!' });
     }
   }
   
--- frontend/popup.tsx
+++ frontend/popup.tsx
@@ -1,35 +1,61 @@
 import { useState } from "react"
+import './styles.css';
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [email, setEmail] = useState('');
+  const [toggle, setToggle] = useState(false);
 
-  const handleStartClick = () => {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      const tabId = tabs[0].id;
-      chrome.tabs.sendMessage(tabId, {message: "startObserving"}, (response) => {
-      });
-    });
-  }
+  const handleEmailButtonClick = () => {
+    console.log('Email submitted:', email);
+  };
 
-  const handleStopClick = () => {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      const tabId = tabs[0].id;
-      chrome.tabs.sendMessage(tabId, {message: "stopObserving"}, (response) => {
+  const handleToggleChange = () => {
+    const newToggleValue = !toggle;
+    setToggle(newToggleValue);
+
+    if (newToggleValue) {
+      chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        const tabId = tabs[0].id;
+        chrome.tabs.sendMessage(tabId, {message: "startObserving"}, (response) => {
+          console.log(response.message)
+        });
       });
-    });
-  }
+    } else {
+      chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        const tabId = tabs[0].id;
+        chrome.tabs.sendMessage(tabId, {message: "stopObserving"}, (response) => {
+          console.log(response.message)
+        });
+      });
+    }
+  };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <button onClick={handleStartClick}>Start</button>
-      <button onClick={handleStopClick}>Stop</button>
+    <div>
+      <div className="header">
+        <h1 className="company-name">Spanglish</h1>
+        <span className="donut-emoji" role="img" aria-label="donut emoji">🍩</span>
+      </div>
+      <div className="container">
+        <div className="email-input">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+          />
+          <button onClick={handleEmailButtonClick}>Submit</button>
+        </div>
+        <div className="switch-container">
+          <label className="switch">
+            <input type="checkbox" checked={toggle} onChange={handleToggleChange} />
+            <span className="slider round"></span>
+          </label>
+          <span className="toggle-label">Turn-on captions for notes</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default IndexPopup
